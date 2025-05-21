a2157222bb0cfd4d1872b35c1bf0cc93c1b214ba
2tankov2 <domino-82@mail.ru>
2025-05-21 17:20:31 +0300

    feat: добавляет фильтрацию данных для защиты от XSS

--- templates/main.php
+++ templates/main.php
@@ -4,7 +4,7 @@
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
         <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
+            <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($category); ?></a>
         </li>
         <?php endforeach; ?>
     </ul>
@@ -17,15 +17,15 @@
         <?php foreach ($lots as $lot): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$lot['img_url'] ?? ''; ?>" width="350" height="260" alt="<?=$lot['name'] ?? ''; ?>">
+                <img src="<?=htmlspecialchars($lot['img_url'] ?? ''); ?>" width="350" height="260" alt="<?=htmlspecialchars($lot['name'] ?? ''); ?>">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?=$lot['category'] ?? ''; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name'] ?? ''; ?></a></h3>
+                <span class="lot__category"><?=htmlspecialchars($lot['category'] ?? ''); ?></span>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($lot['name'] ?? ''); ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?= price_format($lot['price'] ?? 0); ?></span>
+                        <span class="lot__cost"><?= htmlspecialchars(price_format($lot['price'] ?? 0)); ?></span>
                     </div>
                     <div class="lot__timer timer">
                         12:23
