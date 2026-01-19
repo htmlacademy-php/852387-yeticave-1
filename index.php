f2c559d0f9a9a9e3fa04b2f2bd0957cf596a6663
2tankov2 <domino-82@mail.ru>
2026-01-19 19:11:02 +0300

    fix: errors in the text

--- index.php
+++ index.php
@@ -15,39 +15,39 @@ $categories = [
 $ads = [
     [
         'name' => '2014 Rossignol District Snowboard',
-        'categories' => 'Доски и лыжи',
+        'category' => 'Доски и лыжи',
         'price' => 10999,
-        'url' => 'img/lot-1.jpg'
+        'url' => '/img/lot-1.jpg'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'categories' => 'Доски и лыжи',
+        'category' => 'Доски и лыжи',
         'price' => 159999,
-        'url' => 'img/lot-2.jpg'
+        'url' => '/img/lot-2.jpg'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'categories' => 'Крепления',
+        'category' => 'Крепления',
         'price' => 8000,
-        'url' => 'img/lot-3.jpg'
+        'url' => '/img/lot-3.jpg'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charcoal',
-        'categories' => 'Ботинки',
+        'category' => 'Ботинки',
         'price' => 10999,
-        'url' => 'img/lot-4.jpg'
+        'url' => '/img/lot-4.jpg'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charcoal',
-        'categories' => 'Одежда',
+        'category' => 'Одежда',
         'price' => 7500,
-        'url' => 'img/lot-5.jpg'
+        'url' => '/img/lot-5.jpg'
     ],
     [
         'name' => 'Маска Oakley Canopy',
-        'categories' => 'Разное',
+        'category' => 'Разное',
         'price' => 5400,
-        'url' => 'img/lot-6.jpg'
+        'url' => '/img/lot-6.jpg'
     ]
 ];
 ?>
@@ -115,18 +115,18 @@ $ads = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php foreach ($ads as $ad): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?= $ad['url'] ?? ''; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?= $ad['category'] ?? ''; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $ad['name'] ?? ''; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $ad['price'] ?? 0; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -134,6 +134,7 @@ $ads = [
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
