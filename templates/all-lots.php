89541ab063db00f362466ec6ba9d838f252d92a6
2tankov2 <domino-82@mail.ru>
2026-02-05 01:13:54 +0300

    feat(template): add template for display all lots)

--- templates/all-lots.php
+++ templates/all-lots.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
-
 /**
  * @var string[] $categories список категорий лотов
+ * @var string $cat_name название категории
  * @var string[] $lots
  * @var int[] $pages_count количество страниц
  * @var int[] $pages
@@ -14,11 +14,11 @@ declare(strict_types=1);
     <?=include_template('_category.php', ['categories' => $categories]); ?>
     <div class="container">
         <section class="lots">
-            <h2>Результаты поиска по запросу «<span><?=$_GET['search']; ?></span>»</h2>
+            <h2>Все лоты в категории <span>«<?=$cat_name; ?>»</span></h2>
             <?php if (count($lots) > 0): ?>
                 <?=include_template('_lot.php', ['lots' => $lots]); ?>
             <?php else: ?>
-                <p>Ничего не найдено по вашему запросу</p>
+                <p>В этой категории нет лотов</p>
             <?php endif; ?>
         </section>
         <?=include_template('_pagination.php', [
