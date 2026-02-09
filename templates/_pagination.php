0ba137d9af06ef4d7f9135d236fc9e1af29df730
2tankov2 <domino-82@mail.ru>
2026-02-10 01:23:10 +0300

    fix: correct func & @var

--- templates/_pagination.php
+++ templates/_pagination.php
@@ -11,16 +11,16 @@ declare(strict_types=1);
  */
 ?>
 
-<?php if ($pages_count > 1): ?>
+<?php if ($pages_count): ?>
     <ul class="pagination-list">
         <li class="pagination-item pagination-item-prev">
-            <a href="<?= $cur_page > 1 ? create_new_url($path, [$tab => $value, 'page' => $cur_page - 1]) : '#'; ?>">Назад</a></li>
+            <a href="<?= $cur_page > 1 ? create_new_url($path, [$tab => $value, 'page' => $cur_page - 1]) : ''; ?>">Назад</a></li>
         <?php foreach ($pages as $page): ?>
-            <li class="pagination-item <?= $page ===(int)$cur_page ? 'pagination-item-active' : ''; ?>">
+            <li class="pagination-item <?= $page === (int)$cur_page ? 'pagination-item-active' : ''; ?>">
                 <a href="<?= create_new_url($path, [$tab => $value, 'page' => $page]); ?>"><?= $page; ?></a>
             </li>
         <?php endforeach; ?>
         <li class="pagination-item pagination-item-next">
-            <a href="<?= $cur_page < $pages_count ? create_new_url($path, [$tab => $value, 'page' => $cur_page + 1]) : '#'; ?>">Вперед</a></li>
+            <a href="<?= $cur_page < $pages_count ? create_new_url($path, [$tab => $value, 'page' => $cur_page + 1]) : ''; ?>">Вперед</a></li>
     </ul>
 <?php endif; ?>
